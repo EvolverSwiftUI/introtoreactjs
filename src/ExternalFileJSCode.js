83538deb4753b84806662c86d76fe13855ff856d
Sivaram Yadav <sivaramaiah.nakka@concentrix.com>
2022-04-27 12:27:37 +0530

    addons

---
+++ src/ExternalFileJSCode.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { ReactDOM } from "react";
+
+const element = (
+    <div>
+        <h1 className="greeting"> Hello</h1>
+        <p> Welcome to here. </p>
+    </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
